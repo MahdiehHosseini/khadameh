ce9699f4ad6f3418ccb7ffe6bee8ed4ba8595c61
MahdiehHosseini <mahdieh1384.sh@gmail.com>
2024-09-24 10:46:35 +0330

    remove default folders & install unocss

--- khadameh/src/main.ts
+++ khadameh/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import '@unocss/reset/tailwind-compat.css'
+import 'uno.css'
+import './style.css'
 
 createApp(App).mount('#app')
